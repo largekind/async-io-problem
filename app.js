95ef3eff3c463e4bd5750642f7243b9ce589cc4b
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-10-30 20:58:46 +0900

    Update app.js to promise

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+
+//ファイルに処理追加するPromise指定
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+}
+
+// thenで次々と処理を渡す形式ではループ処理は不可なので少しでも同期がずれると変な出力となる
 for (let count = 0; count < 500; count++) {
-  fs.appendFileSync(fileName, 'あ', 'utf8');
-  fs.appendFileSync(fileName, 'い', 'utf8');
-  fs.appendFileSync(fileName, 'う', 'utf8');
-  fs.appendFileSync(fileName, 'え', 'utf8');
-  fs.appendFileSync(fileName, 'お', 'utf8');
-  fs.appendFileSync(fileName, '\n', 'utf8');
+  appendFilePromise(fileName, 'あ')
+    .then(() => {
+      return appendFilePromise(fileName, 'い');
+    })
+    .then(() => {
+      return appendFilePromise(fileName, 'う');
+    })
+    .then(() => {
+      return appendFilePromise(fileName, 'え');
+    })
+    .then(() => {
+      return appendFilePromise(fileName, 'お');
+    })
+    .then(() => {
+      return appendFilePromise(fileName, '\n');
+    });
 }
